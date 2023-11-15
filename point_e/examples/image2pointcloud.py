359646f7e3a7b4d0561bb1ceafc3289245f38cdc
edward1997104 <ka.hei.hui@autodesk.com>
2023-11-15 10:28:08 +0800

    1. update the code for generating point-e

--- point_e/examples/image2pointcloud.py
+++ point_e/examples/image2pointcloud.py
@@ -7,6 +7,7 @@ from point_e.diffusion.sampler import PointCloudSampler
 from point_e.models.download import load_checkpoint
 from point_e.models.configs import MODEL_CONFIGS, model_from_config
 from point_e.util.plotting import plot_point_cloud
+from point_e.util.pc_to_mesh import marching_cubes_mesh
 import trimesh
 import os
 import numpy as np
