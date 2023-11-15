bb331a1283ee3ccf40948f87c29cb1932cb8f1aa
edward1997104 <ka.hei.hui@autodesk.com>
2023-11-15 11:02:55 +0800

    1. update the code for generating point-e

--- point_e/examples/image2pointcloud.py
+++ point_e/examples/image2pointcloud.py
@@ -15,6 +15,20 @@ import math
 import boto3
 import tempfile
 import cloudpathlib
+from dataclasses import dataclass
+import tyro
+import multiprocessing
+
+@dataclass
+class Args:
+    workers : int = 8
+    output_dir : str  = 'pointe-mesh-output'
+    bucket : str = 'gso-renders'
+    ext : str = '018.png'
+    cuda_cnt : int = 8
+
+
+args = tyro.cli(Args)
 
 def scale_to_unit_cube(mesh, scale_ratio = 0.9):
     if isinstance(mesh, trimesh.Scene):
@@ -47,25 +61,30 @@ def rotate_around_axis(mesh, axis = 'x', reverse = False):
 
     return mesh
 
-if __name__ == '__main__':
+def worker(
+    queue: multiprocessing.JoinableQueue,
+    count: multiprocessing.Value,
+    worker_idx : int
+) -> None:
 
-    output_dir = 'pointe-mesh-output'
-    os.makedirs(output_dir, exist_ok=True)
+    while True:
+        item = queue.get()
+        if item is None:
+            break
+        try:
+            process_one(item, worker_idx % args.cuda_cnt)
+        except Exception as e:
+            print(e)
+        queue.task_done()
+        with count.get_lock():
+            count.value += 1
 
-    bucket = 'gso-renders'
-    ext = '018.png'
-    s3 = boto3.resource('s3')
+def process_one(img, cuda_id):
+    torch.cuda.set_device(f'cuda:{cuda_id}')
+    device = torch.device(f'cuda:{cuda_id}')
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(cuda_id)
+    os.environ['BLENDER_PATH'] = args.blender_path
 
-    ## getting file with extensions in s3 bucket
-    bucket = s3.Bucket(bucket)
-    img_lists = []
-    for obj in bucket.objects.filter(Prefix=''):
-        if obj.key.endswith(ext):
-            img_lists.append(obj.key)
-
-    print("Number of images: ", len(img_lists))
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     print('creating base model...')
     base_name = 'base40M'  # use base300M or base1B for better results
@@ -102,30 +121,72 @@ if __name__ == '__main__':
     )
 
     with tempfile.TemporaryDirectory() as tmp_dir:
-        for img in img_lists:
-            print("start processing: ", img)
-            cloudpath = cloudpathlib.CloudPath(f's3://gso-renders/{img}')
-            id = img.split('/')[-3]
-            save_filename = f"{id}.png"
-            save_path = os.path.join(tmp_dir, save_filename)
-            cloudpath.download_to(save_path)
+        print("start processing: ", img)
+        cloudpath = cloudpathlib.CloudPath(f's3://gso-renders/{img}')
+        id = img.split('/')[-3]
+        save_filename = f"{id}.png"
+        save_path = os.path.join(tmp_dir, save_filename)
+        cloudpath.download_to(save_path)
 
-            img = Image.open(save_path)
+        img = Image.open(save_path)
 
-            # Produce a sample from the model.
-            samples = None
-            for x in tqdm(sampler.sample_batch_progressive(batch_size=1, model_kwargs=dict(images=[img]))):
-                samples = x
+        # Produce a sample from the model.
+        samples = None
+        for x in tqdm(sampler.sample_batch_progressive(batch_size=1, model_kwargs=dict(images=[img]))):
+            samples = x
 
-            pc = sampler.output_to_point_clouds(samples)[0]
+        pc = sampler.output_to_point_clouds(samples)[0]
 
-            mesh = marching_cubes_mesh(
-                pc=pc.coords,
-                model=model,
-                batch_size=4096,
-                grid_size=128,  # increase to 128 for resolution used in evals
-                progress=True,
-                fill_vertex_channels=False,
-            )
-            mesh = trimesh.Trimesh(vertices=mesh.verts, faces=mesh.faces)
-            mesh.export(os.path.join(output_dir, f'{id}.obj'))
+        mesh = marching_cubes_mesh(
+            pc=pc.coords,
+            model=model,
+            batch_size=4096,
+            grid_size=128,  # increase to 128 for resolution used in evals
+            progress=True,
+            fill_vertex_channels=False,
+        )
+        mesh = trimesh.Trimesh(vertices=mesh.verts, faces=mesh.faces)
+
+        mesh = mesh.tri_mesh()
+        mesh = trimesh.Trimesh(vertices=mesh.verts, faces=mesh.faces)
+        mesh = rotate_around_axis(mesh, axis='x', reverse=False)
+
+        mesh.export(os.path.join(args.output, f'{id}.obj'))
+
+
+
+if __name__ == '__main__':
+
+    output_dir = 'pointe-mesh-output'
+    os.makedirs(output_dir, exist_ok=True)
+
+    s3 = boto3.resource('s3')
+
+    ## getting file with extensions in s3 bucket
+    bucket = s3.Bucket(args.bucket)
+    img_lists = []
+    for obj in bucket.objects.filter(Prefix=''):
+        if obj.key.endswith(args.ext):
+            img_lists.append(obj.key)
+
+    print("Number of images: ", len(img_lists))
+
+    queue = multiprocessing.JoinableQueue()
+    count = multiprocessing.Value("i", 0)
+
+    for worker_i in range(args.workers):
+        process = multiprocessing.Process(
+            target=worker, args=(queue, count, worker_i)
+        )
+        process.daemon = True
+        process.start()
+
+    for img in img_lists:
+        queue.put(img)
+
+    queue.join()
+
+    for _ in range(args.workers):
+        queue.put(None)
+
+    print(f'Processed {count.value} models')
