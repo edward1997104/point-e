2cefdb2ded9b05aaf51fcf0631ffa64091611eb4
edward1997104 <ka.hei.hui@autodesk.com>
2023-12-09 17:08:23 +0800

    1. update the code for computing pfid

--- point_e/evals/compute_pfid.py
+++ point_e/evals/compute_pfid.py
@@ -3,66 +3,7 @@ import tyro
 from point_e.evals.fid_is import compute_statistics
 import torch
 from point_e.models.download import load_checkpoint
-
-class get_model(nn.Module):
-    def __init__(self, num_class, normal_channel=True, width_mult=1):
-        super(get_model, self).__init__()
-        self.width_mult = width_mult
-        in_channel = 6 if normal_channel else 3
-        self.normal_channel = normal_channel
-        self.sa1 = PointNetSetAbstraction(
-            npoint=512,
-            radius=0.2,
-            nsample=32,
-            in_channel=in_channel,
-            mlp=[64 * width_mult, 64 * width_mult, 128 * width_mult],
-            group_all=False,
-        )
-        self.sa2 = PointNetSetAbstraction(
-            npoint=128,
-            radius=0.4,
-            nsample=64,
-            in_channel=128 * width_mult + 3,
-            mlp=[128 * width_mult, 128 * width_mult, 256 * width_mult],
-            group_all=False,
-        )
-        self.sa3 = PointNetSetAbstraction(
-            npoint=None,
-            radius=None,
-            nsample=None,
-            in_channel=256 * width_mult + 3,
-            mlp=[256 * width_mult, 512 * width_mult, 1024 * width_mult],
-            group_all=True,
-        )
-        self.fc1 = nn.Linear(1024 * width_mult, 512 * width_mult)
-        self.bn1 = nn.BatchNorm1d(512 * width_mult)
-        self.drop1 = nn.Dropout(0.4)
-        self.fc2 = nn.Linear(512 * width_mult, 256 * width_mult)
-        self.bn2 = nn.BatchNorm1d(256 * width_mult)
-        self.drop2 = nn.Dropout(0.4)
-        self.fc3 = nn.Linear(256 * width_mult, num_class)
-
-    def forward(self, xyz, features=False):
-        B, _, _ = xyz.shape
-        if self.normal_channel:
-            norm = xyz[:, 3:, :]
-            xyz = xyz[:, :3, :]
-        else:
-            norm = None
-        l1_xyz, l1_points = self.sa1(xyz, norm)
-        l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
-        l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
-        x = l3_points.view(B, 1024 * self.width_mult)
-        x = self.drop1(F.relu(self.bn1(self.fc1(x))))
-        result_features = self.bn2(self.fc2(x))
-        x = self.drop2(F.relu(result_features))
-        x = self.fc3(x)
-        x = F.log_softmax(x, -1)
-
-        if features:
-            return x, l3_points, result_features
-        else:
-            return x, l3_points
+from point_e.models.pointnet2_cls_ssg import get_model
 
 def normalize_point_clouds(pc: np.ndarray) -> np.ndarray:
     centroids = np.mean(pc, axis=1, keepdims=True)
